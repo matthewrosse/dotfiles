aa3971425958b40f26449db3095b429d98c4a154
Mateusz Różański <rozanski.mateusz00@gmail.com>
2021-03-18 01:41:35 +0100

    AwesomeWM update

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -290,13 +290,13 @@ globalkeys = my_table.join(
              -- {description = "view  previous nonempty", group = "tag"}),
 
     -- Default client focus
-    awful.key({ altkey,           }, "j",
+    awful.key({ altkey,           }, "u",
         function ()
             awful.client.focus.byidx( 1)
         end,
         {description = "focus next by index", group = "client"}
     ),
-    awful.key({ altkey,           }, "k",
+    awful.key({ altkey,           }, "i",
         function ()
             awful.client.focus.byidx(-1)
         end,
