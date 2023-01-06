99c750793dcd9e50cfccd0347078f15671ef87de
Mateusz Różański <rozanski.mateusz00@gmail.com>
2023-01-07 00:41:06 +0100

    zshrc update

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,31 @@
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+# easier to read disk
+alias df='df -h'     # human-readable sizes
+alias free='free -m' # show sizes in MB
+
+# get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4 | head -5'
+# get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3 | head -5'
+
+# Create alias override commands using 'exa'
+alias ls='exa --group-directories-first --icons'
+alias ll='ls -lh --git'
+alias la='ll -a'
+alias tree='ll --tree --level=2'
+
+# navigation
+alias ..='cd ..' 
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
