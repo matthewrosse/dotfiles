24cd88c1f365abb19518cfa6c819a15e48c9012d
Mateusz Różański <rozanski.mateusz00@gmail.com>
2023-03-04 08:21:57 +0100

    Mobile development env vars

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -3,6 +3,10 @@ export TERMINAL="kitty"
 export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
 export GOPATH=$HOME/.local/share/go
 
+export ANDROID_HOME="$HOME/Android/Sdk"
+
+export PATH="$ANDROID_HOME/platform-tools:$PATH"
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
