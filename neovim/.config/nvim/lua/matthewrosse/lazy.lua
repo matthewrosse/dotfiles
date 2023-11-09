562b16deeb3ba47dcb6870de0c1de04bf3da9214
Mateusz Różański <rozanski.mateusz00@gmail.com>
2023-11-09 05:41:38 +0100

    rewrite of neovim config

--- neovim/.config/nvim/lua/matthewrosse/lazy.lua
+++ neovim/.config/nvim/lua/matthewrosse/lazy.lua
@@ -1,21 +1,24 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ 
-  { import = "matthewrosse.plugins" }, 
-  { import = "matthewrosse.plugins.lsp" } 
+require("lazy").setup({
+	{ import = "matthewrosse.plugins" },
+	{ import = "matthewrosse.plugins.lsp" },
 }, {
-  install = {
-    colorscheme = { "catppuccin-macchiato" },
-  },
+	install = {
+		colorscheme = { "catppuccin-macchiato" },
+	},
+	ui = {
+		border = "rounded",
+	},
 })
--- neovim/.config/nvim/lua/matthewrosse/plugins/formatting.lua
+++ neovim/.config/nvim/lua/matthewrosse/plugins/formatting.lua
@@ -1,6 +1,7 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" },
 	cmd = { "ConformInfo" },
 	keys = {
 		{
